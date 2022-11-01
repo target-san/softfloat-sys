75d9cac82f6c51097aeca1fc20347e155b0b4d5a
Igor Baidiuk <igor.baidiuk@tacanslabs.com>
2022-11-01 19:11:40 +0200

    Use bindgen to generate actual fast-int type aliases instead of C99

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,31 @@
 // SPDX-License-Identifier: BSD-3-Clause
 // See Notices.txt for copyright information
-#![cfg_attr(not(test), no_std)]
+//#![cfg_attr(not(test), no_std)]
 #![allow(non_snake_case, non_camel_case_types, non_upper_case_globals)]
 
-extern crate c99;
+#[allow(non_camel_case_types)]
+#[allow(unused)]
+mod internal
+{
+	pub type int8_t = i8;
+	pub type int16_t = i16;
+	pub type int32_t = i32;
+	pub type int64_t = i64;
 
-use c99::{int_fast32_t, int_fast64_t, uint_fast32_t, uint_fast64_t, uint_fast8_t};
+	pub type uint8_t = u8;
+	pub type uint16_t = u16;
+	pub type uint32_t = u32;
+	pub type uint64_t = u64;
+
+    include!(concat!(env!("OUT_DIR"), "/c99-types.rs"));
+}
+
+pub use self::internal::{
+    int8_t, int16_t, int32_t, int64_t,
+    uint8_t, uint16_t, uint32_t, uint64_t,
+    int_fast8_t, int_fast16_t, int_fast32_t, int_fast64_t,
+    uint_fast8_t, uint_fast16_t, uint_fast32_t, uint_fast64_t,
+};
 
 extern "C" {
     pub fn softfloat_detectTininess_read_helper() -> uint_fast8_t;
